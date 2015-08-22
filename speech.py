e04d19794cc4ba18a119892e5638587acef1ce0f
Glenn <invest@eggify.com>
2015-08-22 11:26:37 -0700

    Update speech.py

--- speech.py
+++ speech.py
@@ -10,20 +10,6 @@ import wave
 import pocketsphinx as ps
 import sphinxbase
 
-# Map GPIO block pins to MRAA pin numbers
-# Reference: https://learn.sparkfun.com/tutorials/installing-libmraa-on-ubilinux-for-edison
-pins = collections.OrderedDict()
-pins["GP44"] = 31
-pins["GP45"] = 45
-pins["GP46"] = 32
-pins["GP47"] = 46
-
-# Initialize LED controls
-leds = collections.OrderedDict()
-leds["G"] = mraa.Gpio(pins["GP44"])
-leds["R"] = mraa.Gpio(pins["GP45"])
-leds["W"] = mraa.Gpio(pins["GP46"])
-leds["Y"] = mraa.Gpio(pins["GP47"])
 
 # Parameters for pocketsphinx
 LMD   = "/home/root/led-speech-edison/lm/8484.lm"
@@ -35,44 +21,6 @@ RATE = 16000
 RECORD_SECONDS = 2
 PATH = 'output'
 
-LED_ON = 1
-LED_OFF = 0
-def toggle(led, state):
-    led.write(state)
-
-def allLedsOn(leds):
-    for color in leds:
-        toggle(leds[color], LED_ON)
-
-def allLedsOff(leds):
-    for color in leds:
-        toggle(leds[color], LED_OFF)
-
-def cycleLeds(leds, time_on, num_repeat=1):
-    for i in range(num_repeat):
-        for color in leds:
-            toggle(leds[color], LED_ON)
-            time.sleep(time_on)
-            toggle(leds[color], LED_OFF)
-
-def triggerLeds(leds, words):
-    allLedsOff(leds)
-
-    if "RED" in words:
-        toggle(leds["R"], LED_ON)
-    if "GREEN" in words:
-        toggle(leds["G"], LED_ON)
-    if "WHITE" in words:
-        toggle(leds["W"], LED_ON)
-    if "YELLOW" in words:
-        toggle(leds["Y"], LED_ON)
-    if "ALL" in words:
-        allLedsOn(leds)
-    if "TORONTO" in words:
-        cycleLeds(leds, 0.05)
-    if "CANADA" in words:
-        cycleLeds(leds, 0.1, 2)
-
 def decodeSpeech(speech_rec, wav_file):
 	wav_file = file(wav_file,'rb')
 	wav_file.seek(44)
@@ -82,8 +30,10 @@ def decodeSpeech(speech_rec, wav_file):
 
 def main():
     # Set direction of LED controls to out
+    '''
     for color in leds:
         leds[color].dir(mraa.DIR_OUT)
+    '''
 
     if not os.path.exists(PATH):
         os.makedirs(PATH)
@@ -119,15 +69,16 @@ def main():
     	rec_words = recognised.split()
 
         # Trigger LEDs
-    	triggerLeds(leds, rec_words)
+    	#triggerLeds(leds, rec_words)
 
         # Playback recognized word(s)
     	cm = 'espeak "'+recognised+'"'
-    	os.system(cm)
+        os.system('At your service sir')
+    	#os.system(cm)
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         print "Keyboard interrupt received. Cleaning up..."
-        allLedsOff(leds)
+        #allLedsOff(leds)
